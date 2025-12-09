bcd8f35be5956117059b1937a55ba8e6d9659265
Chirag <113755144+chirag-dua123@users.noreply.github.com>
2025-12-09 20:46:31 +0530

    this is the second commit

---
+++ wordcount.py
@@ -0,0 +1,19 @@
+def wordcount(filename):
+    counts = {}
+    with open(filename, "r") as f:
+        for line in f:
+            words = line.split()
+            for word in words:
+                word = word.lower().strip(".,!?")  # normalize
+                if word in counts:
+                    counts[word] += 1
+                else:
+                    counts[word] = 1
+
+    # Sort by frequency
+    sorted_counts = sorted(counts.items(), key=lambda x: x[1], reverse=True)
+
+    # Print top 10
+    for word, freq in sorted_counts[:10]:
+        print(word, freq)
+
