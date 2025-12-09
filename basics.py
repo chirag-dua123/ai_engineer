3e1ff4a4868fcea46c096b96b6833525701bb0de
Chirag <113755144+chirag-dua123@users.noreply.github.com>
2025-12-09 13:04:23 +0530

    python basics and calculator

---
+++ basics.py
@@ -0,0 +1,19 @@
+def is_even(a):
+    if a%2==0:
+        return True
+    return False
+
+def multiply(a,b):
+    return a*b
+
+def main():
+    assert is_even(10) is True
+    assert is_even(7) is False
+    print("My Name")
+    print(1+2)
+    print([1,2,3,4,5])
+    for i in range(1,21):
+        print(i)
+
+if __name__ == "__main__":
+    main()
